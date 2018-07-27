87965f52716c2d7b730bc330f8e208756a56ef94
huaran <huaran.fan@dmall.com>
2018-07-27 11:41:03 +0800

    init

---
+++ timewheel-base/src/main/java/com/fhr/App.java
@@ -0,0 +1,13 @@
+package com.fhr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
